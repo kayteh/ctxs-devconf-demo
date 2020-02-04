41a2d7ae9a4707951c952953b54d9d70e8a23cc7
Katie Thornhill <katie.thornhill@citrix.com>
2020-02-03 19:43:13 -0500

    http-only example

--- functions/blog--list-posts/go.mod
+++ functions/blog--list-posts/go.mod
@@ -4,8 +4,6 @@ go 1.13
 
 require (
 	cloud.google.com/go/firestore v1.1.1
-	github.com/golang/protobuf v1.3.3
 	github.com/kayteh/ctxs-devconf-demo v0.0.0-20200203231300-98550f07faca
-	github.com/kayteh/ctxs-devconf-rpc v1.5.100
-	google.golang.org/grpc v1.27.0
+	k8s.io/klog v1.0.0
 )
--- functions/blog--list-posts/handler.go
+++ functions/blog--list-posts/handler.go
@@ -2,50 +2,72 @@ package function
 
 import (
 	"context"
+	"encoding/json"
+	"net/http"
 
 	"cloud.google.com/go/firestore"
-	empty "github.com/golang/protobuf/ptypes/empty"
 	pkg "github.com/kayteh/ctxs-devconf-demo/pkg/blog"
-	proto "github.com/kayteh/ctxs-devconf-rpc/blog"
-
-	"google.golang.org/grpc"
+	"k8s.io/klog"
 )
 
 // BlogService is a skeleton RPC type for this function
 type BlogService struct {
-	proto.UnimplementedBlogServer
 	firestore *firestore.Client
 }
 
-// Register the function
-func Register(server *grpc.Server) {
-	proto.RegisterBlogServer(server, &BlogService{
+type BlogPost struct {
+	ID    string `json:"id"`
+	Title string `json:"title"`
+	Body  string `json:"body"`
+}
+
+func Handle(w http.ResponseWriter, r *http.Request) {
+	service := &BlogService{
 		firestore: pkg.NewFirestore(),
-	})
+	}
+
+	jEnc := json.NewEncoder(w)
+
+	posts, err := service.ListPosts()
+	if err != nil {
+		klog.Error(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		jEnc.Encode(map[string]string{"error": "something terrible happened"})
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	jEnc.Encode(posts)
 }
 
 // ListPosts from Firestore
-func (b *BlogService) ListPosts(ctx context.Context, req *empty.Empty) (*proto.PostList, error) {
+func (b *BlogService) ListPosts() ([]BlogPost, error) {
 	rootCollection := b.firestore.Collection("/devconf_blogposts")
-
-	documents, err := rootCollection.Documents(context.Background()).GetAll()
-	if err != nil {
-		return nil, err
+	if rootCollection == nil {
+		return []BlogPost{}, nil
 	}
 
-	posts := make([]*proto.Post, len(documents))
+	documentIterator := rootCollection.Documents(context.Background())
+	if documentIterator == nil {
+		return []BlogPost{}, nil
+	}
+
+	documents, err := documentIterator.GetAll()
+	if err != nil {
+		return []BlogPost{}, err
+	}
+
+	posts := make([]BlogPost, len(documents))
 
 	for idx, document := range documents {
-		post := &proto.Post{
-			Id: document.Ref.ID,
+		post := BlogPost{
+			ID: document.Ref.ID,
 		}
 
-		document.DataTo(post)
+		document.DataTo(&post)
 
 		posts[idx] = post
 	}
 
-	return &proto.PostList{
-		Posts: posts,
-	}, nil
+	return posts, nil
 }
